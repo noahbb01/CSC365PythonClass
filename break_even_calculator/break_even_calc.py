8bac138cd91288c94d3889040143d4984e22fc3e
noahbb01 <nobeeb01@wsc.edu>
2021-09-09 10:55:36 -0500

    Code

--- break_even_calculator/break_even_calc.py
+++ break_even_calculator/break_even_calc.py
@@ -1,5 +1,13 @@
 # !/usr/bin/env python3
 
+"""
+Created by: Noah Beebe, Bryce Youngquist
+Class: CSC365 Scripting Languages
+Date: 9/9/2021
+Break Even Calculator: use this program to figure out to break even on farming
+
+"""
+
 # display a welcome message
 print("Break Even Calculator")
 print()
@@ -19,16 +27,6 @@ earnings = round(total_revenue - total_cost, 3)
 Break_Evan_Cost = round((total_cost - Government_Payment) / Yield, 2)
 per_bushel_profit = round(Price - Break_Evan_Cost, 2)
 
-# format and display the result
-print('=' * 30)
-print()
-print("Total Cost: " + str(total_cost))
-print("Total Revenue: " + str(total_revenue))
-print("Total Earnings: " + str(earnings))
-print("Costs to Break Even: " + str(Break_Evan_Cost))
-print("Per Bushel Profit: " + str(per_bushel_profit))
-print()
-
 # Noah's code for the yield
 print('=' * 30)
 yield2 = Yield * .9
@@ -46,18 +44,6 @@ per_bushel_profit2 = round(Price - Break_Evan_Cost2, 2)
 Break_Evan_Cost3 = round((total_cost - Government_Payment) / yield3, 2)
 per_bushel_profit3 = round(Price - Break_Evan_Cost3, 2)
 
-print("Total Revenue with 10% decrease: " + str(total_revenue2))
-print("Total Earnings with 10% decrease: " + str(earnings2))
-print()
-print("Total Revenue with 10% increase: " + str(total_revenue3))
-print("Total Earnings with 10% increase: " + str(earnings3))
-print()
-print("Costs to Break Even: " + str(Break_Evan_Cost2))
-print("Per Bushel Profit: " + str(per_bushel_profit2))
-print()
-print("Costs to Break Even: " + str(Break_Evan_Cost3))
-print("Per Bushel Profit: " + str(per_bushel_profit3))
-
 # Bryce Code for Cost165
 print('=' * 30)
 print()
@@ -74,18 +60,6 @@ per_bushel_profit4 = round((Price - Break_Evan_Cost4), 2)
 Break_Evan_Cost5 = round((total_cost2 - Government_Payment) / Yield, 2)
 per_bushel_profit5 = round((Price - Break_Evan_Cost5), 2)
 
-print("Total Cost with 10% decrease: " + str(total_revenue4))
-print("Total Cost with 10% decrease: " + str(earnings3))
-print()
-print("Total Cost with 10% increase: " + str(total_revenue5))
-print("Total Cost with 10% increase: " + str(earnings4))
-print()
-print("Costs to Break Even: " + str(Break_Evan_Cost4))
-print("Per Bushel Profit: " + str(per_bushel_profit4))
-print()
-print("Costs to Break Even: " + str(Break_Evan_Cost5))
-print("Per Bushel Profit: " + str(per_bushel_profit5))
-
 print(f'{"":20s}'
       f'{"":15s}'
       f'{"10% Decrease":>15s}'
@@ -93,12 +67,12 @@ print(f'{"":20s}'
       f'{"10% Decrease":>15s}'
       f'{"10% Increase":>15s}')
 
-print(f'{"BreakEven Calculator":20s}'
-      f'{"Per Acre":.15s}'
-      f'{"in Yield":.15s}'
-      f'{"in Yield":.15s}'
-      f'{"in Cost":.15s}'
-      f'{"in Cost":.15s}')
+print(f'{"Break Even Calculator      ":20s}'
+      f'{"Per Acre":15s}'
+      f'{"in Yield":15s}'
+      f'{"in Yield":15s}'
+      f'{"in Cost":15s}'
+      f'{"in Cost":15s}')
 
 print(f'{"-" * 20:>20s}'
       f'{"-" * 13:>15s}'
@@ -107,9 +81,78 @@ print(f'{"-" * 20:>20s}'
       f'{"-" * 13:>15s}'
       f'{"-" * 13:>15s}')
 
+print()
+
+print(f'{"Yield":20s}'
+      f'{Yield:15.2f}'
+      f'{Yield:15.2f}'
+      f'{Yield:15.2f}'
+      f'{Yield:15.2f}'
+      f'{Yield:15.2f}')
+
+print(f'{"Price":20s}'
+      f'{Price:15.2f}'
+      f'{Price:15.2f}'
+      f'{Price:15.2f}'
+      f'{Price:15.2f}'
+      f'{Price:15.2f}')
+
+print(f'{"Gov Payment":20s}'
+      f'{Government_Payment:15.2f}'
+      f'{Government_Payment:15.2f}'
+      f'{Government_Payment:15.2f}'
+      f'{Government_Payment:15.2f}'
+      f'{Government_Payment:15.2f}')
+
+print(f'{"Total Revenue":20s}'
+      f'{total_revenue:15.2f}'
+      f'{total_revenue2:15.2f}'
+      f'{total_revenue3:15.2f}'
+      f'{total_revenue4:15.2f}'
+      f'{total_revenue5:15.2f}')
+
+print()
+
+print(f'{"Variable Cost":20s}'
+      f'{Variable_Cost:15.2f}'
+      f'{Variable_Cost:15.2f}'
+      f'{Variable_Cost:15.2f}'
+      f'{Variable_Cost:15.2f}'
+      f'{Variable_Cost:15.2f}')
+
+print(f'{"Overhead Costs":20s}'
+      f'{overhead_cost:15.2f}'
+      f'{overhead_cost:15.2f}'
+      f'{overhead_cost:15.2f}'
+      f'{overhead_cost:15.2f}'
+      f'{overhead_cost:15.2f}')
+
+print(f'{"Total Cost":20s}'
+      f'{total_cost:15.2f}'
+      f'{total_cost:15.2f}'
+      f'{total_cost:15.2f}'
+      f'{total_cost:15.2f}'
+      f'{total_cost2:15.2f}')
+
+print(f'{"Earnings":20s}'
+      f'{earnings:15.2f}'
+      f'{earnings2:15.2f}'
+      f'{earnings3:15.2f}'
+      f'{earnings4:15.2f}'
+      f'{earnings:15.2f}')
+
+print()
+
+print(f'{"Break Even Price":20s}'
+      f'{Break_Evan_Cost:15.2f}'
+      f'{Break_Evan_Cost2:15.2f}'
+      f'{Break_Evan_Cost3:15.2f}'
+      f'{Break_Evan_Cost4:15.2f}'
+      f'{Break_Evan_Cost5:15.2f}')
+
 print(f'{"Per Bushel Profit":20s}'
-      f'{per_bushel_profit:15,2f}'
-      f'{per_bushel_profit2:15,2f}'
-      f'{per_bushel_profit3:15,2f}'
-      f'{per_bushel_profit4:15,2f}'
-      f'{per_bushel_profit5:15,2f}')
+      f'{per_bushel_profit:15.2f}'
+      f'{per_bushel_profit2:15.2f}'
+      f'{per_bushel_profit3:15.2f}'
+      f'{per_bushel_profit4:15.2f}'
+      f'{per_bushel_profit5:15.2f}')
