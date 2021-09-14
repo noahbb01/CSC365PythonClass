6d0422e761033a03b35685c6d80fcd7fdbec715f
noahbb01 <nobeeb01@wsc.edu>
2021-09-14 10:35:18 -0500

    Code

---
+++ stocking_rate_calculator/stocking_rate_calc.py
@@ -0,0 +1,9 @@
+"""
+Created by: Noah Beebe
+Class: CSC365 Scripting Languages
+Date: 9/14/2021
+Stocking Rate Calculator: use this program to figure out the number of cattle to put in a pasture
+
+"""
+print("Stocking Rate Calculator")
+print('=' * 30)
