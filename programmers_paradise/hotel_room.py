311af18271ff6191ed342c0e02177016d9666a1b
noahbb01 <nobeeb01@wsc.edu>
2021-10-14 10:41:01 -0500

    Trying to figure out surcharge problem

--- programmers_paradise/hotel_room.py
+++ programmers_paradise/hotel_room.py
@@ -16,32 +16,42 @@ __status__ = 'Coding'
 LINE_LENGTH = 55
 
 
-#def surcharge():
-   # if guests == 1
-
-
 def hotel_room():
-    num1 = random.randint(0, 8)
-    num2 = random.randint(0, 10)
-    num3 = random.randint(0, 2)
+    single = random.randint(0, 8)
+    double = random.randint(0, 10)
+    king = random.randint(0, 2)
 
-    single = num1
-    double = num2
-    king = num3
-
-    print(str(num1) + ' single rooms (2 guest max) available at $96')
-    print(str(num2) + ' double rooms (4 guest max) available at $146')
-    print(str(num3) + ' king   rooms (2 guest max) available at $116')
+    print(str(single) + ' single rooms (2 guest max) available at $96')
+    print(str(double) + ' double rooms (4 guest max) available at $146')
+    print(str(king) + ' king   rooms (2 guest max) available at $116')
 
     # add validation for correct room selection
     print()
     print('What type of room do you want to book?')
-    room = str(input('s=Single, d=Double, k=King: '))
+    room = int(input('1=Single, 2=Double, 3=King: '))
     print()
 
     # add validation for guest max
-    guests = str(input('How many guests will be staying in the ' + room + ' room? '))
-    print(str('There is a ' + ))
+    guests = str(input('How many guests will be staying in the ' + str(room) + ' room? '))
+
+    surcharge = 0
+    if guests == 1:
+        surcharge = 0
+    elif guests == 2:
+        surcharge = 10
+    elif guests == 3:
+        surcharge = 18
+    elif guests == 4:
+        surcharge = 32
+
+    total = surcharge + room
+    print(str('There is a $' + str(surcharge) + ' surcharge for ' + str(guests) + ' guests.'))
+    print()
+
+    nights = input('How many nights do you want to book a ' + str(room) + ' room ')
+    print('with ' + str(guests) + ' guests, at ' + str(total) + ' a night? ')
+
+    print('Please confirm booking for ' + str(nights) + ' nights total = ' + str(total) * int(nights))
 
 
 # Can call main to see if it would run without actually running it
