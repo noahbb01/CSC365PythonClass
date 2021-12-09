7b7e62c42032c2ee5f6d38244b3947161e350253
noahbb01 <nobeeb01@wsc.edu>
2021-12-09 09:46:42 -0600

    added 2 new modules and added logic with each one still need more logic

--- final_assignment/main.py
+++ final_assignment/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 """
-Docstring this when ready
+Main code in the blackjack program where the Display rules portion of code is being housed at. I understand that
+it was supposed to include more than one player to play against the dealer but i found it much easier to just do one
+player play against the dealer. All values are held except for the name of the player. It is simply 'Player vs. Dealer'
+For some reason im just curious on is that i do not have anything called in main and it says my imports are not used
+but if i leave them like how they are now the code runs as intended. But if i delete the 'unused imports', the code does
+not run. I understand it a little bit im just gonna leave it because its kinda funny to me anyways.
 
 """
-# import validation
-import gamecode as gc
+import blackjack as bj
+import class_code
 
 __author__ = 'Noah Beebe'
 __copyright__ = 'Copyright 2021, CSC365'
@@ -13,13 +18,28 @@ __version__ = '1.0.1'
 __status__ = 'In Progress'
 
 
+def display_rules():
+    print(f'=========================================================================')
+    print(f'Welcome to Game 21')
+    print(f'=========================================================================')
+    print(f'The rules are simple!')
+    print(f'1. Each player is trying to get as close to 21 without going over.')
+    print(f"2. Each player is ONLY trying to beat the dealer's hand.")
+    print(f'3. The dealer will keep dealing himself cards')
+    print(f'   until he beats all players hands or goes over 21.')
+    print(f'4. Tie goes to the player, not the dealer.')
+    print(f'5. Each player gets dealt two card between 1 - 10. With Ace being high. ')
+    print(f'6. The player then can choose to receive additional cards.')
+    print(f'7. Each player starts with $1.00. (100)')
+    print(f'8. Default bet is however much the player wants to bet.')
+    print(f'9. Winner is the last person with cash, not including the dealer.')
+    print(f'=========================================================================')
+
+
 def main():
     """
-
     :return:
     """
-    gc.display_menu()
-    gc.get_players()
 
 
 if __name__ == '__main__':
